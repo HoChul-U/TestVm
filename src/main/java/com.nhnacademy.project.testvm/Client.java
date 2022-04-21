042ce3571d9b6df1853146a4c833262e8ff09220
sunghwanKim <shkcreate@naver.com>
2022-04-21 16:56:23 +0900

    첫번째 커밋

---
+++ src/main/java/com.nhnacademy.project.testvm/Client.java
@@ -0,0 +1,28 @@
+package com.nhnacademy.project.testvm;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class Client {
+    private final String hostName;
+    private final int port;
+
+    public Client(String hostName, int port) {
+        this.hostName = hostName;
+        this.port = port;
+    }
+
+    public void connect() {
+        try {
+            Socket socket = new Socket(hostName, port);
+
+            Thread sender = new Sender(socket);
+            Thread receiver = new Receiver(socket);
+
+            sender.start();
+            receiver.start();
+        }  catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/com.nhnacademy.project.testvm/Server.java
@@ -0,0 +1,66 @@
+package com.nhnacademy.project.testvm;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private final int port;
+
+    public Server(int port) {
+        this.port = port;
+    }
+
+    public void start() throws IOException {
+        int k = Integer.MIN_VALUE;
+        boolean b = true;
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (b) {
+                k++;
+                b = k < Integer.MAX_VALUE;
+                try {
+                    Socket socket = serverSocket.accept();
+                    ClientSession client = new ClientSession(socket);
+                    client.start();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+    }
+
+    public static class ClientSession extends Thread {
+        private final Socket socket;
+        private final DataInputStream in;
+
+        ClientSession(Socket socket) throws IOException {
+            this.socket = socket;
+            this.in = new DataInputStream(socket.getInputStream());
+        }
+
+        @Override
+        public void run() {
+            connect();
+        }
+
+        private void connect() {
+            if (isConnect()) {
+                try {
+                    Thread sender = new Sender(this.socket);
+                    Thread receiver = new Receiver(this.socket);
+
+                    sender.start();
+                    receiver.start();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+
+        private boolean isConnect() {
+            return this.in != null;
+        }
+    }
+}
